e1e78d766071be5e1e78c8bc58ae8eaf0ebdb8ee
小渕周 <shukobuchi@gmail.com>
2024-08-13 19:34:16 +0900

    動画解析を追加

--- audio.py
+++ audio.py
@@ -1,14 +1,14 @@
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part
 
-project_id = "PROJECT_ID"
+project_id = "PROJECT_ID" # 書き換える
 
 vertexai.init(project=project_id, location="asia-northeast1")
 
 model = GenerativeModel("gemini-1.5-flash-001")
 # model = GenerativeModel("gemini-1.5-pro-001")
 
-audio_file_uri = "gs://bucket/filename.mp3"
+audio_file_uri = "gs://bucket/filename.mp3" # 書き換える
 
 audio_file = Part.from_uri(audio_file_uri, mime_type="audio/mpeg")
 
--- video.py
+++ video.py
@@ -5,16 +5,17 @@ project_id = "PROJECT_ID" # 書き換える
 vertexai.init(project=project_id, location="asia-northeast1")
 
 model = GenerativeModel(model_name="gemini-1.5-flash-001")
+# model = GenerativeModel(model_name="gemini-1.5-pro-001")
 
-image_file_uri = "gs://bucket/filename.png" # 書き換える
-image_file = Part.from_uri(image_file_uri, mime_type="image/png")
+video_file_uri = "gs://bucket/filename.mp4" # 書き換える
+video_file = Part.from_uri(video_file_uri, mime_type="video/mp4")
 
 prompt = """
-  この画像は何ですか？
+  この動画は何ですか？
 """
 
 contents = [
-    image_file,
+    video_file,
     prompt,
 ]
 
